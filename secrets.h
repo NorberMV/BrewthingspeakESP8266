6574c186beb1d86aa4cc3c0173e535ecbcc8437d
NorberMV <42726133+NorberMV@users.noreply.github.com>
2019-12-19 17:49:26 -0500

    This file must be modified!
    Modified the file with  the  private credentials
    >>Thingspeak Channel API key 
    >>MySSID with your WiFi network name
    >>MyPassword with your WiFi password

---
+++ secrets.h
@@ -0,0 +1,13 @@
+
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "UNE_D1E2"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "00002436600804"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 878882
+#define SECRET_WRITE_APIKEY "VJH4QZSVQN6H87OE"
+#define SECRET_CH_ID_WEATHER_STATION 12397	          	//MathWorks weather station
+
+#define SECRET_CH_ID_COUNTER 298725 					//Test channel for counting
+#define SECRET_READ_APIKEY_COUNTER "SODG0O2UZVGKWAWG"	//API Key for Test channel
