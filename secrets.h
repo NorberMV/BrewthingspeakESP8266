c6a5a23571e87af5e48818491b2e9db6fdc8fbfd
Norberto Moreno <nmv245@gmail.com>
2021-03-02 16:16:00 -0500

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -7,7 +7,7 @@
 #define SECRET_PASS "xxxxxxx"	// replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 878882
-#define SECRET_WRITE_APIKEY "VJH4QZSVQN6H87OE"
+#define SECRET_WRITE_APIKEY "VJHXXXXXXXXXXXXX"
 #define SECRET_CH_ID_WEATHER_STATION 12397	          	//MathWorks weather station
 
 #define SECRET_CH_ID_COUNTER 298725 					//Test channel for counting
